85578f67a2dab66b784ef7ba1c9a4af226463983
Marcelo <marcelofoztd@hotmail.com>
2025-02-18 21:34:04 -0300

    adicionado ID do contato

--- src/main/java/app/entity/Contato.java
+++ src/main/java/app/entity/Contato.java
@@ -3,6 +3,7 @@ package app.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.Setter;
 
 public class Contato {
 	
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String telefone;
