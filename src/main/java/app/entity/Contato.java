165c479907c869052077898db6789a61174891a6
Marcus <marcus2607@hotmail.com>
2025-02-24 19:56:40 -0300

    Primeiro commit + boot pro postman

--- src/main/java/app/entity/Contato.java
+++ src/main/java/app/entity/Contato.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ public class Contato {
 	private long id;
 	private String telefone;
 	private String celular;
+	@Email
 	private String email;
 	
 
