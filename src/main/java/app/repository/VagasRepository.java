fa0930c0e240540b3c87fca1308381c352cd1f38
Marcelo <marcelofoztd@hotmail.com>
2025-02-25 20:38:55 -0300

    adicionando findByNome no vagas

--- src/main/java/app/repository/VagasRepository.java
+++ src/main/java/app/repository/VagasRepository.java
@@ -1,9 +1,13 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.entity.Vagas;
 
 public interface VagasRepository extends JpaRepository<Vagas, Long> {
+	
+	public List<Vagas> findByTitulo(String titulo);
 
 }
