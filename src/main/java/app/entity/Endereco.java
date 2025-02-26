4727c832f0a27a5d73c4d6cd6e16eaab8f4cf9c6
Marcus <marcus2607@hotmail.com>
2025-02-25 21:19:38 -0300

    Endereço e Vagas com notblank

--- src/main/java/app/entity/Endereco.java
+++ src/main/java/app/entity/Endereco.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,10 +21,15 @@ public class Endereco {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@NotBlank (message = "Rua não pode estar vazia")
 	private String rua;
+	@NotBlank (message = "Estado não pode estar vazio")
 	private String estado;
+	@NotBlank (message = "Cidade não pode estar vazia")
 	private String cidade;
+	@NotBlank (message = "CEP não pode estar vazia")
 	private String cep;
 	private int numero;
-
+	
+	//relações abaixo
 }
--- src/main/java/app/entity/Vagas.java
+++ src/main/java/app/entity/Vagas.java
@@ -2,10 +2,15 @@ package app.entity;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,15 +27,20 @@ public class Vagas {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@NotBlank (message = "Titulo não pode estar vazio")
 	private String titulo;
+	@NotBlank (message = "Descrição não pode estar vazio")
 	private String descricao;
+	@NotBlank (message = "Salario não pode estar vazio")
 	private float salario;
+	@NotBlank (message = "Requisitos não pode estar vazio")
 	private String requisito;
 	private String setor;
 	private String tipo;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private LocalDate dataAnuncio;
 	private String nivelExperiencia;
-	//teste de breach hahaha tendeu
 	
+	//relações abaixo
 	
 }
